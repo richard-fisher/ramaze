aea15970b45ef79432d0045f32edc4436ccd2a76
Michael Fellinger <m.fellinger@gmail.com>
2011-01-14 14:28:31 +0900

    Make installation of ruby-growl precondition for growl spec

--- spec/ramaze/log/growl.rb
+++ spec/ramaze/log/growl.rb
@@ -1,4 +1,9 @@
 require File.expand_path('../../../../spec/helper', __FILE__)
+
+spec_precondition 'ruby-growl is installed' do
+  require 'ruby-growl'
+end
+
 require 'ramaze/log/growl'
 
 # Configure Growl, make sure your growl server matches
@@ -9,19 +14,19 @@ Ramaze::Logger::Growl.trait[:defaults][:password] = 'ramaze'
 describe Ramaze::Logger::Growl do
   it 'Create a new instance of the Growl logger' do
     growl = Ramaze::Logger::Growl.new
-    
+
     growl.should.respond_to? :log
   end
-  
+
   it 'Send a growl notification' do
     growl = Ramaze::Logger::Growl.new
     growl.log(:info, 'Hello, Ramaze!')
-    
+
     # The Growl library doesn't seem to return anything,
     # let's ask the user if the notification was sent
     print "Did you see the the notification? (y/n) "
     response = gets.strip
-    
+
     response.should.satisfy do |object|
       object === 'y' or object === 'yes'
     end
