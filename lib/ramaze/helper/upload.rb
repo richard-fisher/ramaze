7503d7881a257ed9b0ae68a51d0f241aa3af6f7f
Lars Olsson <lasso@lassoweb.se>
2011-08-08 20:28:04 +0200

    Implement save method, use traits instead of constants, add options for default upload dir, autosave and overwriting existing files.

--- lib/ramaze/helper/upload.rb
+++ lib/ramaze/helper/upload.rb
@@ -6,8 +6,11 @@ module Ramaze
     # by Rack, but this helper adds some conveniance methods for handling
     # and saving the uploaded files.
     module UploadHelper
-
-      DEFAULT_UPLOADED_FILES = {}.freeze
+      
+      include Innate::Traited
+      
+      # Assume that no files have been uploaded by default
+      trait :default_uploaded_files => Hash.new
 
       # This method will iterate through all request parameters
       # and convert those parameters which represents uploaded
@@ -28,7 +31,10 @@ module Ramaze
           if pattern.nil? || pattern =~ k
             if is_uploaded_file?(v)
               uploaded_files[k] = Ramaze::UploadedFile.new(
-                v[:filename], v[:type], v[:tempfile]
+                v[:filename],
+                v[:type],
+                v[:tempfile],
+                ancestral_trait[:upload_options] || ancestral_trait[:default_upload_options]
               )
               request.params.delete(k)
             end
@@ -46,6 +52,12 @@ module Ramaze
               return uploaded_files
             end
           end
+          # Save uploaded files if autosave is set to true
+          if ancestral_trait[:upload_options] && ancestral_trait[:upload_options][:autosave]
+            uploaded_files.each_value do |uf|
+              uf.save
+            end
+          end
         end
       end
 
@@ -56,10 +68,43 @@ module Ramaze
         mod.extend(ClassMethods)
       end
 
+      # Return list of currently handled file uploads
+      def uploaded_files
+        return Innate::Helper::UploadHelper.trait[:default_uploaded_files]
+      end
+
+      private
+
+      # Returns whether +param+ is considered an uploaded file
+      # A parameter is considered to be an uploaded file if it is
+      # a hash and contains all parameters that Rack assigns to an
+      # uploaded file
+      #
+      def is_uploaded_file?(param)
+        if param.is_a?(Hash) &&
+          param.has_key?(:filename) &&
+          param.has_key?(:type) &&
+          param.has_key?(:name) &&
+          param.has_key?(:tempfile) &&
+          param.has_key?(:head)
+          return true
+        else
+          return false
+        end
+      end
+      
       # Helper class methods. Methods in this module will be available
       # in your controller *class* (not your controller instance).
       module ClassMethods
-
+        include Innate::Traited
+        # Default options for uploaded files. You can affect these options
+        # by using the uploads_options method
+        trait :default_upload_options => {
+          :allow_overwrites => false,
+          :autosave => false,
+          :default_upload_dir => nil
+        }
+        
         # This method will activate automatic handling of uploaded files
         # for specified actions
         #
@@ -99,42 +144,14 @@ module Ramaze
             get_uploaded_files(pattern)
           end
         end
-
-        # Set the default save directory for uploaded files. Please note that
-        # no files are actually saved until the save method is called on the
-        # uploaded file.
-        #
-        def default_save_dir=(path)
-          trait({:default_save_dir => path})
+        
+        # Set options for uploads
+        def upload_options(hsh)
+          options = Innate::Helper::UploadHelper::ClassMethods.trait[:default_upload_options].merge(hsh)
+          trait :upload_options => options
         end
-
+        
       end
-
-      # Return list of currently handled file uploads
-      def uploaded_files
-        return DEFAULT_UPLOADED_FILES
-      end
-
-      private
-
-      # Returns whether +param+ is considered an uploaded file
-      # A parameter is considered to be an uploaded file if it is
-      # a hash and contains all parameters that Rack assigns to an
-      # uploaded file
-      #
-      def is_uploaded_file?(param)
-        if param.is_a?(Hash) &&
-          param.has_key?(:filename) &&
-          param.has_key?(:type) &&
-          param.has_key?(:name) &&
-          param.has_key?(:tempfile) &&
-          param.has_key?(:head)
-          return true
-        else
-          return false
-        end
-      end
-
     end
 
   end
@@ -143,18 +160,37 @@ module Ramaze
   class UploadedFile
 
     # Initializes a new Ramaze::UploadedFile object
-    def initialize(filename, type, tempfile)
+    def initialize(filename, type, tempfile, options)
       @filename = filename
       @type = type
       @tempfile = tempfile
+      @options = options
     end
 
     # Save file
     def save(dirname = nil, filename = nil)
-      # Check if both filename and dirname is set and then save
+      # Check that dirname is set
+      dn = dirname || @options[:default_upload_dir]
+      raise Exception.new('Unable to save file, no dirname given') unless dn     
+      # Check that filename is set
+      fn = filename || @filename
+      raise Exception.new('Unable to save file, no filename given') unless fn
+      
+      # Build path
+      path = File.absolute_path(File.join(dn, fn))
+      # Abort if file altready exists and overwrites are not allowed
+      raise Exception.new('Unable to overwrite existing file') if File.exists?(path) && !@options[:allow_overwrites]
+      # Confirm that we can read source file
+      raise Exception.new('Unable to read temporary file') unless File.readable?(@tempfile)
+      # Confirm that we can write to the destination file
+      raise Exception.new("Unable to save file to #{path}. Path is not writable") unless (File.exists?(path) && File.writable?(path)) || File.writable?(File.dirname(path))
+      File.open(@tempfile, 'rb') do |source|
+        File.open(path, 'wb') do |dest|
+          dest.write(source.read)
+        end
+      end
     end
 
   end
-
 
 end
