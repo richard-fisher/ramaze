97278882473f9ef67dc1a7106f6940706a2e15d4
Yorick Peterse <yorickpeterse@gmail.com>
2011-10-21 22:07:23 +0200

    Fixed the remainder of all platform issues.
    Signed-off-by: Yorick Peterse <yorickpeterse@gmail.com>

--- lib/ramaze/log/syslog.rb
+++ lib/ramaze/log/syslog.rb
@@ -4,14 +4,6 @@
 
 require 'syslog'
 
-# Add aliases for the levelnames used by Ramaze logging
-module Syslog
-  alias dev debug
-  alias warn warning
-  alias error err
-  module_function :dev, :warn, :error 
-end
-
 module Ramaze
   module Logger
     ##
@@ -21,19 +13,25 @@ module Ramaze
     class Syslog
       include Logging
 
+      # Hash containing various method aliases. Rbx and Jruby don't seem to like
+      # the combination of alias() and module_function() so this works around
+      # that.
+      ALIASES = {:dev => :debug, :warn => :warning, :error => :err}
+
       ##
       # Open the syslog library, if it is allready open, we reopen it using the
-      # new argument list.  The argument list is passed on to the Syslog library
-      # so please check that, and man syslog for detailed information.  
+      # new argument list. The argument list is passed on to the Syslog library
+      # so please check that, and man syslog for detailed information.
+      #
       # There are 3 parameters:
       #
-      #   ident:  The identification used in the log file, defaults to $0
-      #   options:  defaults to  Syslog::LOG_PID | Syslog::LOG_CONS
-      #   facility: defaults to Syslog::LOG_USER
+      # * ident:  The identification used in the log file, defaults to $0
+      # * options:  defaults to  Syslog::LOG_PID | Syslog::LOG_CONS
+      # * facility: defaults to Syslog::LOG_USER
       #
-      def initialize( *args )
+      def initialize(*args)
         ::Syslog.close if ::Syslog.opened?
-        ::Syslog.open( *args )
+        ::Syslog.open(*args)
       end
 
       ##
@@ -41,8 +39,15 @@ module Ramaze
       # We simply return if the log was closed for some reason, this behavior
       # was copied from Informer.  We do not handle levels here. This will
       # be done by te syslog daemon based on it's configuration.
+      #
       def log(tag, *messages)
         return if !::Syslog.opened?
+        tag = tag.to_sym
+
+        if ALIASES.key?(tag)
+          tag = ALIASES[tag]
+        end
+
         ::Syslog.send(tag, *messages)
       end
 
