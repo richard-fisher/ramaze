c64833cbf37494780df906eae031354889aca147
Michael Fellinger <m.fellinger@gmail.com>
2011-12-28 03:58:55 +0100

    fix shadowed variables in Thread#into

--- lib/ramaze/snippets/thread/into.rb
+++ lib/ramaze/snippets/thread/into.rb
@@ -4,13 +4,13 @@
 # Extensions for Thread
 class Thread
   # Copy all thread variables into the new thread
-  def self.into *args
-    Thread.new(Thread.current, *args) do |thread, *args|
+  def self.into(*args)
+    Thread.new(Thread.current, *args) do |thread, *thread_args|
       thread.keys.each do |k|
         Thread.current[k] = thread[k] unless k.to_s =~ /^__/
       end
 
-      yield(*args)
+      yield(*thread_args)
     end
   end
 end # Thread
