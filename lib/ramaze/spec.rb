b3662f057b9749d20faf23e0cc7df9bfa9f780c2
Yorick Peterse <yorickpeterse@gmail.com>
2012-12-07 00:10:37 +0100

    Removed ramaze/spec.rb.
    This file has been deprecated for a long time.
    
    Signed-off-by: Yorick Peterse <yorickpeterse@gmail.com>

--- lib/ramaze/spec.rb
+++
@@ -1,4 +0,0 @@
-require File.expand_path('../', __FILE__) unless defined?(Ramaze)
-
-Ramaze.deprecated "require('ramaze/spec')", "require('ramaze/spec/bacon')"
-require File.expand_path('../spec/bacon', __FILE__)
